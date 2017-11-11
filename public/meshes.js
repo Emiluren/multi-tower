684fe0d523cd32fbdc75b28407d0e02aaf154033
Robin Sliwa <rorrosliwa@gmail.com>
2017-11-11 15:41:27 +0100

    started adding materials to meshes

--- public/meshes.js
+++ public/meshes.js
@@ -1,8 +1,9 @@
 var meshData = {};
 
+
 var meshLoadList = [
-  ['castle', 'assets/Castle.obj'],
-  ['test', 'assets/Castle.obj']
+  ['castle', 'assets/Castle'],
+  ['test', 'assets/Castle']
 ];
 
 function loadOBJFiles() {
@@ -14,32 +15,39 @@ function loadOBJFiles() {
   return new Promise(function(resolve, reject) {
     meshLoadList.forEach(function(mesh) {
       let name = mesh[0];
-      let url = mesh[1];
-      loader.load(
-      	// resource URL
-      	url,
-      	// called when resource is loaded
-      	function ( object ) {
-            meshData[name] = [object.children[0].geometry, object.children[0].material];
-            
-            if (Object.values(meshData).every((x) => x != null)) {
-                resolve(meshData);
-            }
-      	},
-      	// called when loading is in progresses
-      	function ( xhr ) {
-
-      		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-      	},
-      	// called when loading has errors
-      	function ( error ) {
-
-      		console.log( 'An error happened' );
-
-      	}
-        
-      );
+      let url = mesh[1] + ".obj";
+      let mtlUrl = mesh[1] + ".mtl";
+      
+      mtlLoader.load(mtlUrl, function(materials) {
+         materials.preload();
+         objLoader.setMaterials(materials);
+         
+         objLoader.load(
+             // resource URL
+             url,
+             // called when resource is loaded
+             function ( object ) {
+                 console.log("materials:",materials);
+                 meshData[name] = [object.children[0].geometry, object.children[0].material];
+                 
+                 if (Object.values(meshData).every((x) => x != null)) {
+                     resolve(meshData);
+                 }
+             },
+             // called when loading is in progresses
+             function ( xhr ) {
+                 
+                 console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+                 
+             },
+             // called when loading has errors
+             function ( error ) {
+                 
+                 console.log( 'An error happened' );
+                 
+             }
+         );
+      });
     });
   });
 }
