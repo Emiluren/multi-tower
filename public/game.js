674d794f395b2b69a60cac2255a9624a4ce51e8d
AXEL-LAPTOP\AxelWickman <axelwickm@gmail.com>
2017-11-11 19:45:46 +0100

    Some raycasting

--- public/game.js
+++ public/game.js
@@ -1,9 +1,15 @@
-var momentum = new THREE.Vector3(0, 0, 0);
+var raycaster = new THREE.Raycaster();
+var mouse_pos = new THREE.Vector2();
 
+
+var momentum = new THREE.Vector3(0, 0, 0);
 var acceleration_rate = 1;
 var deacceleration_rate = 0.5;
 
 function update(delta) {
+
+    // Moving camera
+
   var speed = delta / 100.0;
   var acceleration = new THREE.Vector3(0,0,0);
 
@@ -20,12 +26,28 @@ function update(delta) {
   if (down("descend"))
       acceleration.y -= speed*acceleration_rate;
 
-  console.log(acceleration);
-
   momentum = momentum.add(acceleration).multiplyScalar(Math.pow(deacceleration_rate , speed));
   camera.position.x += momentum.x*speed
   camera.position.y += momentum.y*speed
   camera.position.z += momentum.z*speed
+
+
+    // Getting mouse mosition
+    $(document).mousemove(function(event) {
+        mouse_pos.x =  ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse_pos.y =  ( event.clientY / window.innerWidth ) * 2 - 1;
+    });
+
+    // Raycast on mouse click
+    $(document).click(function () {
+        raycaster.setFromCamera( mouse_pos, camera );
+        console.log(mouse_pos);
+        var intersects = raycaster.intersectObjects( scene.children );
+        console.log(intersects);
+        for ( var i = 0; i < intersects.length; i++ ) {
+            intersects[ i ].object.material.color.set( 0xff0000 );
+        }
+    })
 }
 
 function draw() {
