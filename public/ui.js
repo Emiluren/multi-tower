ef97ad9ea60f7c39a32a7d74f9fe212d42fd4480
AXEL-LAPTOP\AxelWickman <axelwickm@gmail.com>
2017-11-11 22:11:53 +0100

    Creation menue

--- public/ui.js
+++ public/ui.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+    // Modal
+    $('.ui.modal').modal('setting', 'closable', false);
+    if (!debug) $('.ui.modal').modal('show');
 
+    $('#player_name_submit').click(function () {
+        if ($('#player_name_text').val()){
+            console.log("Player "+$('#player_name_text').val()+" enters");
+            $('.ui.modal').modal('hide')
+        }
+    });
+
+    // Sidebar
     $('.sidebar')
         .sidebar({
             closable: false,
@@ -9,27 +20,24 @@ $(document).ready(function(){
         .sidebar('attach events','#sidebar_toggle')
         .sidebar('toggle');
 
-    $('.ui.modal').modal('setting', 'closable', false);
-    if (!debug) $('.ui.modal').modal('show');
-
+    // Sidebar toggle
     $("#sidebar_toggle").state({text: {inactive:'<<', active:'>>'}});
+
     // THIS IS A REALLY UGLY WAY DO DO IT. IT SHOULD BE TRANSITIONING INSTEAD
     setInterval(function () {
         $("#sidebar_toggle").css("left", ($(".sidebar").position().left+$(".sidebar").width())+"px");
     }, 10);
 
-    $('#player_name_submit').click(function () {
-        if ($('#player_name_text').val()){
-            console.log("Player "+$('#player_name_text').val()+" enters");
-            $('.ui.modal').modal('hide')
-        }
-    });
-
+    // Healthbar
     function setHealth(value){
         $('#healthbar_container').progress({ percent: value });
         $('#healthbar_text').text(value+"%");
     }
     setHealth(30);
+
+    // Add button - events
+    $('#create_tower').click(function(){ adding = 'building';  });
+    $('#create_minion').click(function(){ adding = 'minion';  });
 
 
 });
