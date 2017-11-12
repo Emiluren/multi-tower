cafc8cb52a7f916e829c0718804d0a6e594a7e1d
Alfred Sporre <alfred@familjensporre.se>
2017-11-12 11:43:00 +0100

    WTF

--- public/projectile.js
+++ public/projectile.js
@@ -4,14 +4,14 @@ var projectiles = [];
 function getEntityPosition(id) {
     let xPos = entities[id].x;
     let zPos = entities[id].y;
-    
+
     return [xPos, zPos];
 }
 
 function drawProjectile(towerId, targetId) {
     let towerPos = getEntityPosition(towerId);
     let targetPos = getEntityPosition(targetId);
-    
+
     var geometry = new THREE.SphereGeometry( 0.1, 32, 32 );
     var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
     var sphere = new THREE.Mesh( geometry, material );
@@ -29,19 +29,20 @@ function updateProjectiles(delta) {
                 let projectilePos = projectile.mesh.position;
                 let dist = targetV3Pos.sub(projectilePos);
                 let dir = dist.normalize();
-                
+
                 projectilePos.add(dir.multiplyScalar(delta / 100));
-                
+
             if (projectiles.indexOf(projectile) == projectiles.length-1) {
                 resolve("All projectiles updated!");
             }
-        });
-    });
+          }
+      });
+  });
 }
 
 function deleteProjectiles(message) {
     projectiles = projectiles.filter(function(projectile) {
-        
+
         let projectilePos = projectile.mesh.position;
         let targetPos = getEntityPosition(projectile.targetId);
         let targetV3Pos = new THREE.Vector3(targetPos[0], 0, targetPos[1]);
